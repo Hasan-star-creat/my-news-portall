26cec97865f971b0029def474287ba4c59b7fcde
Hasan Alli <hasanali3150927@gmail.com>
2021-05-26 02:43:42 +0600

    site resposive added

--- src/components/Dettails/Dettails.js
+++ src/components/Dettails/Dettails.js
@@ -1,19 +1,33 @@
 import React from 'react';
-import { Button, Card, Title } from 'react-bootstrap';
+import { Button, Card, Row, Container, Col } from 'react-bootstrap';
+
 
 const Dettails = (props) => {
     const {title,urlToImage, author, description} = props.articles;
     return (
         <div>
-             <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src={urlToImage} />
-            <Card.Body>
-                <Card.Title> {title}</Card.Title>
-                <Card.Text> {description}  </Card.Text>
-                <Card.Text className="fst-italic text-primary">Author: {author}  </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
+        <div class="container">
+        <Card style={{ width:'50rem' }}>
+        <div class="row">
+            <div class="col-6 col-sm-8  col-md-11"><Card.Img height="200px" variant="top" src={urlToImage} /></div>
+        </div>
+        <Card.Body>
+        <div class="row">
+            <div class="col-6 col-sm-8 col-md-11 col-lg-12"><Card.Title> {title}</Card.Title></div>
+        </div>
+        <div class="row">
+            <div class="col-6 col-sm-8 col-md-11"><Card.Text> {description} </Card.Text></div>
+        </div>
+        <div class="row">
+            <div class="col-6 col-sm-8 col-md-12"><Card.Text className="fst-italic text-primary">Author: {author}  </Card.Text></div>
+        </div>
+        <div class="row">
+            <div class="col-6 col-sm-8 col-md-11"><Button variant="primary">Go somewhere</Button></div>
+        </div>
   </Card.Body>
-</Card>  
+  </Card>  
+  </div>
+
         </div>
     );
 };
